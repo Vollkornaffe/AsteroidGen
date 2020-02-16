7ddf075eba2594d24f7fc7ba40178f4afedb09e0
Lars Helge Scheel <lars.helge.scheel@rwth-aachen.de>
2020-02-16 17:48:47 +0100

    Added settings

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 use std::env;
 
+fn print_example() {
+    println!("Example usage: ");
+    println!("cargo run 1000 1000");
+    panic!()
+}
+
+struct Settings {
+    imgx: u32,
+    imgy: u32,
+}
+
+impl Settings {
+    fn new(args: Vec<String>) -> Self {
+
+        if args.len() < 3 {
+            println!("Not enough arguments given.");
+            print_example();
+        }
+
+        let imgx = args[1].parse::<u32>().expect("imgx couldn't be parsed");
+        let imgy = args[2].parse::<u32>().expect("imgy couldn't be parsed");
+      
+        Self {
+            imgx,
+            imgy,
+        }
+    }
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
-    let imgx = 800;
-    let imgy = 800;
+    let settings = Settings::new(env::args().collect());
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
+    let scalex = 3.0 / settings.imgx as f32;
+    let scaley = 3.0 / settings.imgy as f32;
 
     // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let mut imgbuf = image::ImageBuffer::new(settings.imgx, settings.imgy);
 
     // Iterate over the coordinates and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
@@ -21,8 +47,8 @@ fn main() {
     }
 
     // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
+    for x in 0..settings.imgx {
+        for y in 0..settings.imgy {
             let cx = y as f32 * scalex - 1.5;
             let cy = x as f32 * scaley - 1.5;
 
