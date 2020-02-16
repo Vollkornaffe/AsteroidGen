47c8231a1fe85cd8789c4bb4366fe268cf8945ed
Lars Helge Scheel <lars.helge.scheel@rwth-aachen.de>
2020-02-16 18:15:02 +0100

    Got rid of the example code.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::env;
 
 fn print_example() {
     println!("Example usage: ");
-    println!("cargo run 1000 1000");
+    println!("cargo run imgx imgy scale");
+    println!("cargo run 100 100 1.0");
     panic!()
 }
 
 struct Settings {
     imgx: u32,
     imgy: u32,
+    scale: f32,
+    outname: String,
 }
 
 impl Settings {
@@ -21,52 +24,44 @@ impl Settings {
 
         let imgx = args[1].parse::<u32>().expect("imgx couldn't be parsed");
         let imgy = args[2].parse::<u32>().expect("imgy couldn't be parsed");
+        let scale = args[3].parse::<f32>().expect("scale couldn't be parsed");
+        let outname = args[4].clone();
       
         Self {
             imgx,
             imgy,
+            scale,
+            outname,
         }
     }
+}
+
+fn to_color_rgb(r: f32, g: f32, b: f32) -> [u8; 3] {
+    [
+        (255.0 * r) as u8,
+        (255.0 * g) as u8,
+        (255.0 * b) as u8,
+    ]
 }
 
 fn main() {
 
     let settings = Settings::new(env::args().collect());
 
-    let scalex = 3.0 / settings.imgx as f32;
-    let scaley = 3.0 / settings.imgy as f32;
-
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(settings.imgx, settings.imgy);
 
     // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([r, 0, b]);
-    }
+    for (i, j, pixel) in imgbuf.enumerate_pixels_mut() {
+        let x = settings.scale * ((i as f32 + 0.5) / settings.imgx as f32 - 0.5);
+        let y = settings.scale * ((j as f32 + 0.5) / settings.imgy as f32 - 0.5);
 
-    // A redundant loop to demonstrate reading image data
-    for x in 0..settings.imgx {
-        for y in 0..settings.imgy {
-            let cx = y as f32 * scalex - 1.5;
-            let cy = x as f32 * scaley - 1.5;
-
-            let c = num_complex::Complex::new(-0.4, 0.6);
-            let mut z = num_complex::Complex::new(cx, cy);
-
-            let mut i = 0;
-            while i < 255 && z.norm() <= 2.0 {
-                z = z * z + c;
-                i += 1;
-            }
-
-            let pixel = imgbuf.get_pixel_mut(x, y);
-            let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], i as u8, data[2]]);
+        //*pixel = image::Rgb([x as u8 ,y as u8, 0]);
+        if x * x + y * y < 0.25 {
+            *pixel = image::Rgb(to_color_rgb(x.abs(),y.abs(),0.0));
         }
     }
 
-    // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save("fractal.png").unwrap();
+    // Save the image
+    imgbuf.save(settings.outname).unwrap();
 }
