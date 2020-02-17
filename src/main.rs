46a209af7dd47b92e1ad2ba02adde48001ea46d0
Lars Helge Scheel <lars.helge.scheel@rwth-aachen.de>
2020-02-17 17:38:41 +0100

    Simple graph visualization

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,24 @@ fn print_example() {
 struct Settings {
     imgx: u32,
     imgy: u32,
-    scale: f32,
-    node_size: f32,
-    edge_size: f32,
     outname: String,
 }
 
 impl Settings {
     fn new(args: Vec<String>) -> Self {
 
-        if args.len() < 3 {
+        if args.len() < 4 {
             println!("Not enough arguments given.");
             print_example();
         }
 
         let imgx = args[1].parse::<u32>().expect("imgx couldn't be parsed");
         let imgy = args[2].parse::<u32>().expect("imgy couldn't be parsed");
-        let scale = args[3].parse::<f32>().expect("scale couldn't be parsed");
-        let node_size = args[4].parse::<f32>().expect("node_size couldn't be parsed");
-        let edge_size = args[5].parse::<f32>().expect("edge_size couldn't be parsed");
-        let outname = args[6].clone();
+        let outname = args[3].clone();
       
         Self {
             imgx,
             imgy,
-            scale,
             outname,
         }
     }
@@ -53,6 +46,7 @@ struct Node {
     pos_y: f32,
     size: f32,
 }
+
 struct Edge {
     a: usize,
     b: usize,
@@ -82,45 +76,45 @@ impl Graph {
             panic!("Invalid a or b for graph! #nodes: {}, a: {}, b: {}", self.nodes.len(), a, b);
         }
 
-        self.edges(push(Edge { a, b, size }));
+        self.edges.push(Edge { a, b, size });
     }
 
     fn dist_to_nodes(&self, q_x: f32, q_y: f32) -> f32 {
-        for node in self.nodes {
+        let mut min_dist = std::f32::MAX;
+        for node in &self.nodes {
             let dist_x = q_x - node.pos_x;
             let dist_y = q_y - node.pos_y;
-            let dist = (dist_x*dist_x + dist_y*dist_y).sqrt() - nodes.size;
+            let dist = (dist_x*dist_x + dist_y*dist_y).sqrt() - node.size;
+            if dist < min_dist {
+                min_dist = dist;
+            }
         }
+        min_dist
     }
 
     fn dist_to_edges(&self, q_x: f32, q_y: f32) -> f32 {
-        for edge in self.edges {
+        let mut min_dist = std::f32::MAX;
+        for edge in &self.edges {
             let a_x = self.nodes[edge.a].pos_x;
             let a_y = self.nodes[edge.a].pos_y;
             let b_x = self.nodes[edge.b].pos_x;
             let b_y = self.nodes[edge.b].pos_y;
-            let d_a_x = q_x - a_x;
-            let d_a_y = q_y - a_y;
-            let d_b_x = q_x - b_x;
-            let d_b_y = q_y - b_y;
+            let qa_x = q_x - a_x;
+            let qa_y = q_y - a_y;
+            let ba_x = b_x - a_x;
+            let ba_y = b_y - a_y;
             let h = num::clamp(
-                () / (),
-                0.0,
-                1.0,
+                (qa_x * ba_x + qa_y * ba_y) / (ba_x * ba_x + ba_y * ba_y),
+                0.0, 1.0,
             );
+            let dist_x = qa_x - ba_x * h;
+            let dist_y = qa_y - ba_y * h;
+            let dist = (dist_x * dist_x + dist_y * dist_y).sqrt() - edge.size;
+            if dist < min_dist {
+                min_dist = dist;
+            }
         }
-
-        float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
-        {
-            vec3 pa = p - a;
-            vec3 ba = b - a;
-            float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
-            return length( pa - ba*h ) - r;
-        }
-        let dist_x = node.pos_x - q_x;
-        let dist_y = node.pos_y - q_y;
-        let dist = (dist_x*dist_x + dist_y*dist_y).sqrt() - nodes.size;
-
+        min_dist
     }
 }
 
@@ -130,22 +124,29 @@ fn main() {
 
     let mut graph = Graph::new();
 
-    graph.add_node(0.0, 0.0);
-    graph.add_node(0.0, 0.5);
-    graph.add_edge(0, 1);
+    graph.add_node(0.0, 0.0, 0.01);
+    graph.add_node(0.1, 0.1, 0.01);
+    graph.add_node(0.1, 0.0, 0.01);
+    graph.add_node(0.0, 0.1, 0.01);
+    graph.add_edge(0, 1, 0.005);
+    graph.add_edge(1, 1, 0.005);
+    graph.add_edge(0, 1, 0.005);
+    graph.add_edge(2, 1, 0.005);
+    graph.add_edge(3, 1, 0.005);
 
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(settings.imgx, settings.imgy);
 
     // Iterate over the coordinates and pixels of the image
     for (i, j, pixel) in imgbuf.enumerate_pixels_mut() {
-        let x = settings.scale * ((i as f32 + 0.5) / settings.imgx as f32 - 0.5);
-        let y = settings.scale * ((j as f32 + 0.5) / settings.imgy as f32 - 0.5);
+        let x = (i as f32 + 0.5) / settings.imgx as f32 - 0.5;
+        let y = (j as f32 + 0.5) / settings.imgy as f32 - 0.5;
 
-
-
-        if x * x + y * y < 0.25 {
-            *pixel = image::Rgb(to_color_rgb(x.abs(),y.abs(),0.0));
+        if graph.dist_to_edges(x,y) < 0.0 {
+            *pixel = image::Rgb(to_color_rgb(0.0,0.0,1.0));
+        }
+        if graph.dist_to_nodes(x,y) < 0.0 {
+            *pixel = image::Rgb(to_color_rgb(1.0,0.0,0.0));
         }
     }
 
